ecb7b791093ebf1afa48832debe13094f4878572
Purpursarkans <goga.danilov10@gmail.com>
2021-09-05 19:00:25 +0400

    update

--- Example.cpp
+++ Example.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 #include "Update.hpp"
 
+#define UrlToUpdate "https://raw.githubusercontent.com/Purpursarkans/update/main/version"
 #define VERSION 2
+#define NameProgram "Example"
 
 int main(int argc, char * argv[])
 {
-    update(argc, argv, "https://raw.githubusercontent.com/Purpursarkans/update/main/version", VERSION, "Example");
+    update(argc, argv, UrlToUpdate, VERSION, NameProgram);
 
     std::cout << "all update" << std::endl;
 
