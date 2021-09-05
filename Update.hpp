12410219d2c5ef3424286df1b6498ac27acc602f
igorek <goga.danilov10@gmail.com>
2021-09-05 20:30:35 +0400

    Update Update.hpp

--- Update.hpp
+++ Update.hpp
@@ -3,7 +3,7 @@
 
 #include <bits/stdc++.h>
 
-void update(int argc, char *argv[], std::string sVersionUrl, int VERSION, std::string exeName)
+void update(int argc, char *argv[], std::string NameFileVersion, std::string sVersionUrl, int VERSION, std::string exeName)
 {
     static std::string exeNameCopy = exeName + "2";
 
@@ -30,10 +30,10 @@ void update(int argc, char *argv[], std::string sVersionUrl, int VERSION, std::s
         }
     }
 
-    std::string downloadVersion = "curl -o version -L " + sVersionUrl;
+    std::string downloadVersion = "curl -o " + NameFileVersion + " -L " + sVersionUrl;
     system(downloadVersion.c_str());
 
-    std::ifstream rFile("version");
+    std::ifstream rFile(NameFileVersion);
     std::string versionS;
     std::string downloadFileUrl;
     std::getline(rFile, versionS);
